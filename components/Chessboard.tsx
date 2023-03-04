876329d2f9d76cdfec6dd9ddd89627647cfd3263
Pawel Niewiadowski <speakpolishpl@gmail.com>
2023-03-04 17:11:15 +0100

    make chessboard

--- components/Chessboard.tsx
+++ components/Chessboard.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import styles from "@/styles/Chessboard.module.css";
 import { startCastleMovement, whiteBottom } from "@/constants/Constans";
+import Tile from "./Tile";
 
 export default function Chessboard() {
   const [figurePlacement, setFigurePlacement] = useState(whiteBottom);
@@ -12,5 +13,13 @@ export default function Chessboard() {
   const [castleMovementTrack, setCastleMovementTrack] =
     useState(startCastleMovement);
 
-  return <div className={styles.container}>Chessboard</div>;
+  return (
+    <div className={styles.container}>
+      {[...Array(8)].map((v, i) =>
+        [...Array(8)].map((w, j) => (
+          <Tile id={8 * i + j} row={i} col={j} key={8 * i + j} />
+        ))
+      )}
+    </div>
+  );
 }
--- components/Tile.tsx
+++ components/Tile.tsx
@@ -1,5 +1,21 @@
 import React from "react";
+import styles from "@/styles/Chessboard.module.css";
+import { type } from "os";
 
-export default function Tile() {
-  return <div>Tile</div>;
+type Props = {
+  id: number;
+  row: number;
+  col: number;
+};
+
+export default function Tile({ id, row, col }: Props) {
+  const isBlack = Boolean(Math.abs(row - col) % 2);
+  return (
+    <div
+      className={styles.tile}
+      style={{ backgroundColor: isBlack ? "darkorange" : "white" }}
+    >
+      {id}
+    </div>
+  );
 }
